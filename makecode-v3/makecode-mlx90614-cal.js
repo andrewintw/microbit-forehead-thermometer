9cc0d3d67205051244790f22b55100f68b9e5862
andrew.lin <andrew_lin@gemteks.com>
2020-03-16 22:51:36 +0800

    add v3 -- support calibration

---
+++ makecode-v3/makecode-mlx90614-cal.js
@@ -0,0 +1,143 @@
+function _1初始化 () {
+    _設定變數()
+    _顯示歡迎畫面()
+}
+function _2顯示偵測額溫 () {
+    _多次採樣取平均溫度()
+    _顯示溫度()
+}
+function _3處理取得的溫度 () {
+    if (校正後的溫度 <= 發燒溫度) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+        basic.showIcon(IconNames.Sad)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+}
+function _設定變數 () {
+    發燒溫度 = 37.5
+    溫度校正值 = 0
+    採樣次數 = 5
+    A程序處理中 = 0
+    B程序處理中 = 0
+    校正模式中 = 0
+}
+function _多次採樣取平均溫度 () {
+    for (let index = 0; index <= 採樣次數 - 1; index++) {
+        _取得感測器溫度()
+        _LED進度條(Math.map(index, 0, 採樣次數, 0, 4))
+        溫度累加值 = 溫度累加值 + 偵測到的溫度
+    }
+    偵測到的溫度 = 溫度累加值 / 採樣次數
+    溫度累加值 = 0
+}
+function _顯示歡迎畫面 () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+}
+function _取得感測器溫度 () {
+    偵測到的溫度 = MLX90614.temperature(TemperatureLocation.Object)
+}
+function _LED進度條 (rows: number) {
+    basic.clearScreen()
+    for (let ledX = 0; ledX <= 4; ledX++) {
+        led.plot(ledX, 4 - rows)
+    }
+    basic.pause(80)
+}
+input.onButtonPressed(Button.A, function () {
+    if (校正模式中 == 1) {
+        溫度校正值 += -0.5
+    } else {
+        A程序處理中 = 1
+        basic.clearScreen()
+        _2顯示偵測額溫()
+        _3處理取得的溫度()
+        A程序處理中 = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (校正模式中 == 1) {
+        溫度校正值 += 0.5
+    } else {
+        if (A程序處理中 == 0) {
+            B程序處理中 = 1
+            basic.clearScreen()
+            basic.showNumber(溫度校正值)
+            B程序處理中 = 0
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    校正模式中 = 1 - 校正模式中
+    if (校正模式中 == 1) {
+        basic.clearScreen()
+        basic.showString("CAL")
+    } else {
+        動畫進行中 = 1
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . # . . .
+            `)
+        basic.showIcon(IconNames.Yes)
+        動畫進行中 = 0
+    }
+})
+function _顯示溫度 () {
+    basic.clearScreen()
+    校正後的溫度 = 偵測到的溫度 + 溫度校正值
+    if (校正後的溫度 >= 0) {
+        basic.showString(convertToText(校正後的溫度).substr(0, 5))
+    } else {
+        basic.showString(convertToText(校正後的溫度).substr(0, 6))
+    }
+}
+let 動畫進行中 = 0
+let 偵測到的溫度 = 0
+let 溫度累加值 = 0
+let 校正模式中 = 0
+let B程序處理中 = 0
+let A程序處理中 = 0
+let 採樣次數 = 0
+let 溫度校正值 = 0
+let 發燒溫度 = 0
+let 校正後的溫度 = 0
+_1初始化()
+basic.forever(function () {
+    if (校正模式中 == 1) {
+        basic.clearScreen()
+        basic.showNumber(溫度校正值)
+    } else {
+        if (A程序處理中 == 0 && 動畫進行中 == 0) {
+            basic.showLeds(`
+                . . . # .
+                . . # . .
+                . # # # #
+                . . # . .
+                . . . # .
+                `)
+            basic.showArrow(ArrowNames.West)
+        }
+    }
+})
